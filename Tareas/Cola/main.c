d280276dddc038da0fb8052a030cb6fae79a9368
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-11-27 13:09:40 -0600

    Error en la insercion de datos
    Falta depurar para encontrar el error

--- Tareas/Cola/main.c
+++ Tareas/Cola/main.c
@@ -1,19 +1,21 @@
 #include "cola.h"
 
-int main()
+int
+main ()
 {
-	struct Cola *cola=NULL;
-	cola=crearCola(cola);
-	cola=enqueue(cola,1);
-	cola=enqueue(cola,2);
-	cola=enqueue(cola,3);
-	cola=enqueue(cola,4);
-	cola=enqueue(cola,5);
-	mostrar(cola);
-	printf("\n");
-	cola=dequeue(cola);
-	mostrar(cola);
-	return 0;
-	
-	
+  struct Cola *cola = NULL;
+  crearCola (cola);
+
+  cola = enqueue (cola, 1);
+  cola = enqueue (cola, 2);
+  cola = enqueue (cola, 3);
+  cola = enqueue (cola, 4);
+  cola = enqueue (cola, 5);
+  mostrar (cola);
+  printf ("\n");
+  cola = dequeue (cola);
+  mostrar (cola);
+  return 0;
+
+
 }
