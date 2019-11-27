1a58de48924dd519d420d64bec4e40a212560e62
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-11-27 13:10:59 -0600

    Update cola.h

--- Tareas/Cola/cola.h
+++ Tareas/Cola/cola.h
@@ -3,15 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Cola{
-	int dato;
-	struct Cola *ultimo;
-	
+struct Cola
+{
+  int dato;
+  struct Cola *ultimo;
+
 };
 
-struct Cola *crearCola(struct Cola *cola);
-struct Cola *eliminarCola(struct Cola *cola
-struct Cola *enqueue(struct Cola * cola, int dato);
-struct Cola *dequeue(struct Cola * cola);
-struct Cola *mostrar(struct Cola * cola);
+struct Cola *crearCola (struct Cola *cola);
+struct Cola *eliminarCola (struct Cola *cola);
+struct Cola *enqueue (struct Cola *cola, int dato);
+struct Cola *dequeue (struct Cola *cola);
+struct Cola *mostrar (struct Cola *cola);
 #endif
