5201f973e45b2cf180b6d3618bc31d25b4549bec
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-11-27 12:46:58 -0600

    main de la cola

---
+++ Tareas/Cola/main.c
@@ -0,0 +1,19 @@
+#include "cola.h"
+
+int main()
+{
+	struct Cola *cola=NULL;
+	cola=crearCola(cola);
+	cola=enqueue(cola,1);
+	cola=enqueue(cola,2);
+	cola=enqueue(cola,3);
+	cola=enqueue(cola,4);
+	cola=enqueue(cola,5);
+	mostrar(cola);
+	printf("\n");
+	cola=dequeue(cola);
+	mostrar(cola);
+	return 0;
+	
+	
+}
