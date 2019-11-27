87b5bec272d5568bc3d232ddc204d5afc005fa4e
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-11-27 13:10:24 -0600

    posible error en funcion insertar

--- Tareas/Cola/cola.c
+++ Tareas/Cola/cola.c
@@ -1,63 +1,93 @@
 #include "cola.h"
 
-struct Cola *crearCola(struct Cola *cola){
-	struct Cola *cola=NULL;
-	
+struct Cola *
+crearCola (struct Cola *cola)
+{
+  struct Cola *Cola = NULL;
+  Cola = (struct Cola *) malloc (sizeof (struct Cola));
+
 }
-struct Cola *eliminarCola(struct Cola *cola{
-	struct Cola *cola=NULL;
-	if((Cola==NULL)
+
+struct Cola *
+eliminarCola (struct Cola *cola)
+{
+  struct Cola *Cola = NULL;
+  if (Cola == NULL)
+    {
+      return NULL;
+    }
+  else
+    {
+      struct Cola *tmp = Cola;
+      Cola = Cola->ultimo;
+      free (tmp);
+    }
+  return cola;
+}
+
+struct Cola *
+enqueue (struct Cola *cola, int dato)
+{
+  if (cola == NULL)
+    {
+      cola = (struct Cola *) malloc (sizeof (struct Cola));
+      cola->dato = dato;
+      cola->ultimo = NULL;
+    }
+  else
+    {
+      struct Cola *tmp = cola;
+      struct Cola *anterior = NULL;
+      while (cola != NULL)
 	{
-		return;
-	}else{
-		struct Cola *tmp=cola;
-		cola=cola->ultimo;
-		free(tmp);
-		eliminar(cola);
+	  anterior = cola;
+	  cola = cola->ultimo;
 	}
-	return cola;
+      cola = (struct Cola *) malloc (sizeof (struct Cola));
+      cola->dato = dato;
+      cola->ultimo = NULL;
+      anterior->ultimo = cola;
+      cola = tmp;
+
+    }
+  return cola;
 }
-struct Cola *enqueue(struct Cola * cola, int dato){
-	struct Cola *cola=NULL;
-	if(cola==NULL){
-		cola=(struct Cola*)malloc(sizeof(struct Cola));
-cola->dato=dato;
-cola->ultimo=NULL;		
-	}else{
-		struct Cola *tmp=cola;
-		struct Cola *anterior=NULL;
-		while(cola!=NULL)
-		{
-			anterior=cola;
-			cola=cola->ultimo;
-		}
-		cola=(struct Cola*)malloc(sizeof(struct Cola));
-		cola->dato=dato;
-		cola->ultimo=NULL;
-		anterior->ultimo=cola;
-		cola=tmp;
-		
-	}
+
+struct Cola *
+dequeue (struct Cola *cola)
+{
+  struct Cola *Cola = NULL;
+  if (Cola == NULL)
+    {
+      printf ("Cola vacia");
+    }
+  else
+    {
+      struct Cola *tmp = Cola;
+      Cola = Cola->ultimo;
+      free (tmp);
+    }
+  return cola;
 }
-struct Cola *dequeue(struct Cola * cola){
-	struct Cola *cola=NULL;
-	if(cola==NULL)
+
+struct Cola *
+mostrar (struct Cola *cola)
+{
+  struct Cola *Cola = NULL;
+
+  if (Cola == NULL)
+    {
+      printf ("\n");
+    }
+  else
+    {
+      struct Cola *cola = Cola;
+      while (cola != NULL)
 	{
-		printf("Cola vacia");
+	  printf ("%d->", cola->dato);
+	  cola->ultimo = cola;
 	}
-	else{
-		struct Cola *tmp=cola;
-		cola=cola->ultimo;
-		free(tmp);
-	}
-	return cola;
-}
-struct Cola *mostrar(struct Cola * cola){
-	if(cola==NULL)
-	{
-			printf("\n");
-	}else{
-		printf("%d->",cola->dato);
-		mostrar(cola->dato);
-		return cola;
+
+    }
+  return cola;
 }
