aa8989f52a147d3485680dc36c772153cf7c507c
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-11-27 12:45:55 -0600

    Se creo cola.h

---
+++ Tareas/Cola/cola.h
@@ -0,0 +1,17 @@
+#ifndef COLA__H__
+#define COLA__H__
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Cola{
+	int dato;
+	struct Cola *ultimo;
+	
+};
+
+struct Cola *crearCola(struct Cola *cola);
+struct Cola *eliminarCola(struct Cola *cola
+struct Cola *enqueue(struct Cola * cola, int dato);
+struct Cola *dequeue(struct Cola * cola);
+struct Cola *mostrar(struct Cola * cola);
+#endif
